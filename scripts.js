74ace30d20e5cd297f6b400c5dcea4b30cd380b4
Bhupendra Singh <danubhupi@gmail.com>
2022-01-08 16:02:48 +0530

    Scrub Added

--- scripts.js
+++ scripts.js
@@ -22,50 +22,53 @@ togglePlay = () => {
   }
 };
 
-
-skipVid=(event)=>{
-    if(event.target.id==='skip'){
-        video.currentTime=video.currentTime+25;
+skipVid = (event) => {
+  if (event.target.id === "skip") {
+    video.currentTime = video.currentTime + 25;
+  } else {
+    if (video.currentTime > 10) {
+      video.currentTime = video.currentTime - 10;
     }
-    else{
-        if(video.currentTime>10)
-        {
-            video.currentTime=video.currentTime-10;
-        }
-    }
-}
+  }
+};
 
-sliderChange=(event)=>{
-   
-    if(event.target.name==="volume"){
-        video.volume=ranges[0].value;
-    }
-    else{
-        video.playbackRate=ranges[1].value;
-    }
-}
+sliderChange = (event) => {
+  if (event.target.name === "volume") {
+    video.volume = ranges[0].value;
+  } else {
+    video.playbackRate = ranges[1].value;
+  }
+};
 
-handleProgress=()=>{
-    console.log("hey");
-    let percent=(video.currentTime/video.duration)*100;
+handleProgress = () => {
+  let percent = (video.currentTime / video.duration) * 100;
 
-    progressBar.style.flexBasis=`${percent}%`;
-}
+  progressBar.style.flexBasis = `${percent}%`;
+};
 
+scrubForward = (event) => {
+  if (!click) return;
+  let percent = (event.offsetX / progress.clientWidth) * 100;
+  video.currentTime = (percent * video.duration) / 100;
+  progressBar.style.flexBasis = `${percent}%`;
+};
 
-let click=false;
+let click = false;
 
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 toggle.addEventListener("click", togglePlay);
-document.getElementById('rewind').addEventListener('click',skipVid);
-document.getElementById('skip').addEventListener('click',skipVid);
-ranges.forEach(slider=>slider.addEventListener('mousemove',sliderChange));
-// ranges.forEach(slider=>slider.addEventListener('click',()=>{click=true; 
-ranges.forEach(slider=>slider.addEventListener('change',sliderChange));
-video.addEventListener('timeupdate',handleProgress);
-
-
-
-
+document.getElementById("rewind").addEventListener("click", skipVid);
+document.getElementById("skip").addEventListener("click", skipVid);
+ranges.forEach((slider) => slider.addEventListener("mousemove", sliderChange));
+// ranges.forEach(slider=>slider.addEventListener('click',()=>{click=true;
+ranges.forEach((slider) => slider.addEventListener("change", sliderChange));
+video.addEventListener("timeupdate", handleProgress);
+progress.addEventListener("mousedown", () => {
+  click = true;
+});
+progress.addEventListener("mouseup", () => {
+  click = false;
+});
+progress.addEventListener("mousemove", scrubForward);
